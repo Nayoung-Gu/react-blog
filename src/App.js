b5818ba05167e6a83709620e1b2e911dbf41e18f
Nayoung Gu <skdud000322@naver.com>
2022-06-18 23:13:38 +0900

    Feat : 홈 화면과 글 화면 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./components/page/MainPage";
+import PostView from "./components/page/PostView";
+
 function App() {
-	return <>React</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/post-view" element={<PostView />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
