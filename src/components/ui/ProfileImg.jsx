c801817e648fd19a54a0d4bf45a227943c3e26b0
Nayoung Gu <skdud000322@naver.com>
2022-06-19 21:29:33 +0900

    Feat: 사이즈별 프로필 이미지 컴포넌트 생성

---
+++ src/components/ui/ProfileImg.jsx
@@ -0,0 +1,13 @@
+import "./profileImg.css";
+
+export function ProfileImg_sm() {
+  return <div className="profile-img sm"></div>;
+}
+
+export function ProfileImg_md() {
+  return <div className="profile-img md"></div>;
+}
+
+export function ProfileImg_lg() {
+  return <div className="profile-img lg"></div>;
+}
