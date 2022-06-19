2ba2af3fd953077bb613bc0556a2b187f6eb7f86
Nayoung Gu <skdud000322@naver.com>
2022-06-19 21:27:44 +0900

    Feat: footer 컴포넌트 생성

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import MoveTopBtn from "./MoveTopBtn";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <h2>&copy; Weniv Corp.</h2>
+        <MoveTopBtn />
+      </footer>
+    </div>
+  );
+}
