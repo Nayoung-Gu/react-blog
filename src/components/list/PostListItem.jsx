0668373e26353015466a407cd69e2ee15ad1482f
Nayoung Gu <skdud000322@naver.com>
2022-06-19 21:30:32 +0900

    Feat: 메인 페이지 및 목록 컴포넌트 생성

--- src/components/list/PostListItem.jsx
+++ src/components/list/PostListItem.jsx
@@ -1,9 +1,48 @@
 import { useNavigate } from "react-router-dom";
+import CategoryBtn from "../ui/CategoryBtn";
+import img from "../../assets/images/post-img1.jpg";
+import { ProfileImg_sm } from "../ui/ProfileImg";
+import "./PostListItem.css";
 
-function PostListItem() {
+function PostListItem(props) {
   const navigate = useNavigate();
 
-  return <div onClick={() => navigate("/post-view")}>포스트입니다.</div>;
+  return (
+    <article className="card" onClick={() => navigate("/post-view")}>
+      <div className="img-container">
+        <img src={img} alt="" />
+      </div>
+      <div className="contents-wrap">
+        <CategoryBtn />
+        <h3 className="card-title">
+          Loremr sit, amet consectetur adipisicing elit. Similique neque fuga
+          rem dolor eligendi aute
+        </h3>
+        <div className="card-detail">
+          <ProfileImg_sm />
+          <span className="writer">Chilli</span>
+          <span className="date">2022.05.25</span>
+        </div>
+        <p className="card-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+          cumque. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Similique neque fuga rem dolor eligendi autem saepe quis a dolorum
+          ullam quia assumenda soluta maiores, sit perspiciatis tenetur libero
+          molestias iurm aliquam alias maiores suscipit voluptatibus dolorum
+          doloribus et non quasi tempore. Repudiandae. Iusto reiciendis
+          explicabo dolor, quae quidem a harum eieque fuga rem dolor eligendi
+          autem saepe quis a dolorum ullam quia assumenda soluta maiores, sit
+          perspiciatis tenetur libero molestias iurm aliquam alieque fuga rem
+          dolor eligendi autem saepe quis a dolorum ullam quia assumenda soluta
+          maiores, sit perspiciatis tenetur libero molestias iurm aliquam
+          alieque fuga rem dolor eligendi autem saepe quis a dolorum ullam quia
+          assumenda soluta maiores, sit perspiciatis tenetur libero molestias
+          iurm aliquam alius, assumenda deleniti deserunt incidunt? Expedita
+          corrupti hic eos fugiat. Possimus molestias facilis perspicia
+        </p>
+      </div>
+    </article>
+  );
 }
 
 export default PostListItem;
--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import PostListItem from "../list/PostListItem";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import "./MainPage.css";
 
 function MainPage() {
   return (
     <div>
+      <Header />
       MainPage입니다.
       <PostListItem></PostListItem>
+      <Footer></Footer>
     </div>
   );
 }
