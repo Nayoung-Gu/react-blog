8e69733121fc6d3cd56a643b66d5e57888918694
Nayoung Gu <skdud000322@naver.com>
2022-06-20 21:45:13 +0900

    Feat: 헤더 컴포넌트 생성 및 json 데이터 삽입

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,15 @@
-import Logo from "./Logo";
-import { ProfileImg_md } from "../ui/ProfileImg";
-import WriteBtn from "./WriteBtn-lg";
-import LogoutBtn from "./LogoutBtn-lg";
+import data from "../../assets/data.json";
 import "./Header.css";
 
 export default function Header() {
   return (
     <header>
-      <Logo />
-      <div>
-        <ProfileImg_md />
-        <WriteBtn />
-        <LogoutBtn />
+      <div className="header-container">
+        <div className="header-wrapper">
+          <h2 className="header__subtitle">{data.blog.subTitle}</h2>
+          <h1 className="header__maintitle">{data.blog.mainTitle}</h1>
+          <p className="header__description">{data.blog.description}</p>
+        </div>
       </div>
     </header>
   );
