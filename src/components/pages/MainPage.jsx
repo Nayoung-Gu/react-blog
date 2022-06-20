91cd8955e556f75b4f0895764e6d5994fefeddc8
Nayoung Gu <skdud000322@naver.com>
2022-06-20 21:50:08 +0900

    Feat: 메인 페이지 컴포넌트 조합

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import PostListItem from "../list/PostListItem";
+import Nav from "../nav/Nav";
 import Header from "../header/Header";
+import About from "../about/About";
 import Footer from "../footer/Footer";
 import "./MainPage.css";
 
 function MainPage() {
   return (
     <div>
+      <Nav />
       <Header />
-      MainPage입니다.
+      <About />
       <PostListItem></PostListItem>
       <Footer></Footer>
     </div>
