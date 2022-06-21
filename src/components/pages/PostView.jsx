74ef5aa4063ae7d05bf44b2831391cebb6e10145
Nayoung Gu <skdud000322@naver.com>
2022-06-21 22:19:32 +0900

    Feat: 블로그 글 화면 구현

--- src/components/pages/PostView.jsx
+++ src/components/pages/PostView.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import Header from "../header/Header";
-import PreviousBtn from "../ui/PreviousBtn";
-import ModifyBtn from "../ui/ModifyBtn";
-import DeleteBtn from "../ui/DeleteBtn";
-import LikeBtn from "../ui/LikeBtn";
+import PostDetail from "../list/PostDetail";
 import Footer from "../footer/Footer";
 
 function PostView() {
   return (
     <div>
       <Header />
-      <PreviousBtn />
-      <ModifyBtn />
-      <DeleteBtn />
-      <LikeBtn />
-      PostView페이지입니다.
+      <PostDetail />
       <Footer />
     </div>
   );
