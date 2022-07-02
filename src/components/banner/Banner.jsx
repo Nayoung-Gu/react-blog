1287ca2664d90b102fd766fe0859fe2b4f409b7f
Nayoung-Gu <skdud000322@naver.com>
2022-07-02 18:56:12 +0900

    Style: 레이아웃 일부 수정

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,14 +4,16 @@ import "./banner.css";
 export default function Banner() {
     return (
         <div className="banner">
-            <div className="banner-container">
-                <div className="banner-wrapper">
-                    <h2 className="banner-subtitle">{data.blog.subTitle}</h2>
-                    <h1 className="banner-maintitle">{data.blog.mainTitle}</h1>
-                    <p className="banner-description">
-                        {data.blog.description}
-                    </p>
-                </div>
+            <img
+                src="/assets/background.jpg"
+                alt=""
+                className="banner-background"
+            />
+
+            <div className="banner-wrapper">
+                <h2 className="banner-subtitle">{data.blog.subTitle}</h2>
+                <h1 className="banner-maintitle">{data.blog.mainTitle}</h1>
+                <p className="banner-description">{data.blog.description}</p>
             </div>
         </div>
     );
